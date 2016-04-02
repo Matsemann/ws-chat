22642a73d30ad161ffe4185a66dcb1cababaea54
Mats Krüger Svensson <mats@matsemann.com>
2016-04-03 00:56:52 +0200

    basic ws-backend

--- index.js
+++ index.js
@@ -8,22 +8,85 @@ app.use(express.static(__dirname + "/client"));
 
 var server = http.createServer(app);
 server.listen(port);
-
 console.log("http server listening on %d", port);
 
-var wss = new WebSocketServer({server: server});
+var websocketServer = new WebSocketServer({server: server});
 console.log("websocket server created");
 
-wss.on("connection", function (ws) {
-    var id = setInterval(function () {
-        ws.send(JSON.stringify(new Date()), function () {
-        })
-    }, 1000);
+websocketServer.on("connection", function (ws) {
+    console.log(`[CONNECTED] ${ws._socket.remoteAddress}/${ws.username} connected`);
 
-    console.log("websocket connection open");
+    ws.on('message', function (stringData) {
+        var data = JSON.parse(stringData);
+
+        if (data.type === 'join') {
+            userJoined(data.name, ws);
+        } else if (data.type === 'message') {
+            messageSent(data.message, ws);
+        }
+    });
+
 
     ws.on("close", function () {
-        console.log("websocket connection close");
-        clearInterval(id);
+        userLeft(ws);
     })
 });
+
+
+function sendToAll(data) {
+    websocketServer.clients.forEach(function (ws) {
+        ws.send(JSON.stringify(data));
+    });
+}
+
+function userJoined(name, ws) {
+    ws.username = name;
+    console.log(`[JOINED] ${ws._socket.remoteAddress}/${ws.username} joined`);
+
+    var activeUsers = getActiveUsers();
+
+    var userData = {
+        type: 'users',
+        action: 'joined',
+        name: name,
+        users: activeUsers
+    };
+
+    sendToAll(userData);
+}
+
+function userLeft(ws) {
+    if (!ws.username) {
+        return; // user never joined with a name
+    }
+
+    console.log(`[LEFT] ${ws._socket.remoteAddress}/${ws.username} left`);
+    var activeUsers = getActiveUsers();
+
+    var userData = {
+        type: 'users',
+        action: 'left',
+        name: ws.username,
+        users: activeUsers
+    };
+
+    sendToAll(userData);
+}
+
+function getActiveUsers() {
+    return websocketServer.clients.map(function (ws) {
+        return ws.username;
+    });
+}
+
+function messageSent(message, ws) {
+    console.log(`[MESSAGE] ${ws._socket.remoteAddress}/${ws.username} sent: ${message}`);
+
+    var messageData = {
+        type: 'message',
+        name: ws.username,
+        message: message
+    };
+
+    sendToAll(messageData);
+}
