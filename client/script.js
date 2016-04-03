070dbae34d983b0faf4125bd0c7bc63bd5b60f02
Mats Krüger Svensson <mats@matsemann.com>
2016-04-03 15:26:46 +0200

    tester kjapt på heroku

--- client/script.js
+++ client/script.js
@@ -1,4 +1,4 @@
-var websocketServerUrl = 'ws://localhost:5000';
+var websocketServerUrl = 'ws://bekk-chat.herokuapps.com';
 var websocket = new WebSocket(websocketServerUrl);
 
 var messageElement = document.querySelector('#messages');
